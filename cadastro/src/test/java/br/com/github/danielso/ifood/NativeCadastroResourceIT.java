cfc309d1c57fb34d4933e453bbe3a333f5a9f1bc
daniel.oliveira <danielso2007@gmail.com>
2021-06-16 00:42:42 -0300

    feat: Criação e configuração do projeto (#1)

---
+++ cadastro/src/test/java/br/com/github/danielso/ifood/NativeCadastroResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.github.danielso.ifood;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCadastroResourceIT extends CadastroResourceTest {
+
+    // Execute the same tests but in native mode.
+}
