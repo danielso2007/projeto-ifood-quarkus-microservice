6461d898d5fcecd8a7bf5fb785ec4e3a2eac8c3f
daniel.oliveira <danielso2007@gmail.com>
2021-06-23 23:10:03 -0300

    test: Finalizado todos os testes de cadastro (#8)

--- cadastro/src/test/java/br/com/github/danielso/ifood/resources/NativeCadastroResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.github.danielso.ifood.resources;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCadastroResourceIT extends CadastroResourceTest {
-
-    // Execute the same tests but in native mode.
-}
