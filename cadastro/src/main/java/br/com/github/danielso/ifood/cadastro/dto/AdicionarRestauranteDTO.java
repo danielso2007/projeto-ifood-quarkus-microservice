f90e01d6928ee2966e06258196cfaf0d071e7b70
daniel.oliveira <danielso2007@gmail.com>
2021-06-24 01:31:49 -0300

    feat(restaurante): usando mapstruct para a entidade restaurante (#9)

--- cadastro/src/main/java/br/com/github/danielso/ifood/cadastro/dto/AdicionarRestauranteDTO.java
+++ cadastro/src/main/java/br/com/github/danielso/ifood/cadastro/dto/AdicionarRestauranteDTO.java
@@ -1,58 +1,45 @@
-package br.com.github.danielso.ifood.cadastro.entities;
+package br.com.github.danielso.ifood.cadastro.dto;
 
 import java.util.Objects;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
 
-@Entity
-@Table(name = "restaurante", indexes = {@Index(name = "indexRestauranteNome", columnList = "nome, cnpj")}, uniqueConstraints = @UniqueConstraint(columnNames = "nome"))
-public class Restaurante extends BaseAudit {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AdicionarRestauranteDTO {
+
+	@Schema(description = "O nome do proprietário do Restaurante", example = "Daniel Oliveira")
 	@NotEmpty(message = "O nome do proprietário não pode ser vazio")
 	@NotNull(message = "O nome do proprietário não pode ser nulo")
 	@Length(min = 2, max = 300)
-	@Column(length = 300, nullable = false)
 	private String proprietario;
+	@Schema(description = "CNPJ do Restaurante (sem pontuação)", example = "76115833000160")
 	@NotEmpty(message = "O CNPJ do proprietário não pode ser vazio")
 	@NotNull(message = "O CNPJ do proprietário não pode ser nulo")
-	@Min(value = 14, message = "CNPJ não pode ser menor que 14 caracteres")
 	@Length(min = 14, max = 14, message = "CNPJ deve ter 14 números")
 	@Pattern(regexp = "^[0-9]{14}$", message = "CNPJ inválido")
-	@Column(length = 14, nullable = false)
+	@CNPJ
 	private String cnpj;
+	@Schema(description = "O nome do Restaurante", example = "Outback")
 	@NotEmpty(message = "O nome não pode ser vazio")
 	@NotNull(message = "O nome não pode ser nulo")
 	@Length(min = 2, max = 300)
-	@Column(length = 300, nullable = false)
 	private String nome;
+	@Schema(description = "A localização do Restaurante")
+	@NotNull(message = "A localizacao deve ser informada")
+	private AdicionarLocalizacaoDTO localizacao;
 
-	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "localizacao_id", referencedColumnName = "id")
-	private Localizacao localizacao;
-
-	@OneToMany(mappedBy = "restaurante", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private Set<Prato> pratos;
-
-	public Restaurante() {
+	public AdicionarRestauranteDTO() {
 	}
 
-	public Restaurante(String proprietario, String cnpj, String nome, Localizacao localizacao) {
+	public AdicionarRestauranteDTO(String proprietario, String cnpj, String nome, AdicionarLocalizacaoDTO localizacao) {
 		this.proprietario = proprietario;
 		this.cnpj = cnpj;
 		this.nome = nome;
@@ -83,38 +70,30 @@ public class Restaurante extends BaseAudit {
 		this.nome = nome;
 	}
 
-	public Localizacao getLocalizacao() {
+	public AdicionarLocalizacaoDTO getLocalizacao() {
 		return this.localizacao;
 	}
 
-	public void setLocalizacao(Localizacao localizacao) {
+	public void setLocalizacao(AdicionarLocalizacaoDTO localizacao) {
 		this.localizacao = localizacao;
 	}
 
-	public Set<Prato> getPratos() {
-		return pratos;
-	}
-
-	public void setPratos(Set<Prato> pratos) {
-		this.pratos = pratos;
-	}
-
-	public Restaurante proprietario(String proprietario) {
+	public AdicionarRestauranteDTO proprietario(String proprietario) {
 		setProprietario(proprietario);
 		return this;
 	}
 
-	public Restaurante cnpj(String cnpj) {
+	public AdicionarRestauranteDTO cnpj(String cnpj) {
 		setCnpj(cnpj);
 		return this;
 	}
 
-	public Restaurante nome(String nome) {
+	public AdicionarRestauranteDTO nome(String nome) {
 		setNome(nome);
 		return this;
 	}
 
-	public Restaurante localizacao(Localizacao localizacao) {
+	public AdicionarRestauranteDTO localizacao(AdicionarLocalizacaoDTO localizacao) {
 		setLocalizacao(localizacao);
 		return this;
 	}
@@ -135,20 +114,18 @@ public class Restaurante extends BaseAudit {
 		if (!super.equals(obj)) {
 			return false;
 		}
-		if (!(obj instanceof Restaurante)) {
+		if (!(obj instanceof AdicionarRestauranteDTO)) {
 			return false;
 		}
-		Restaurante other = (Restaurante) obj;
+		AdicionarRestauranteDTO other = (AdicionarRestauranteDTO) obj;
 		return Objects.equals(cnpj, other.cnpj) && Objects.equals(localizacao, other.localizacao)
 				&& Objects.equals(nome, other.nome) && Objects.equals(proprietario, other.proprietario);
 	}
 
 	@Override
 	public String toString() {
-		return "Restaurante [getProprietario()=" + getProprietario() + ", getCnpj()=" + getCnpj() + ", getNome()="
-				+ getNome() + ", getLocalizacao()=" + getLocalizacao() + ", getPratos()=" + getPratos()
-				+ ", hashCode()=" + hashCode() + ", getDataCriacao()=" + getDataCriacao() + ", getDataAtualizacao()="
-				+ getDataAtualizacao() + ", getId()=" + getId() + "]";
+		return "AdicionarRestauranteDTO [proprietario=" + proprietario + ", cnpj=" + cnpj + ", nome=" + nome
+				+ ", localizacao=" + localizacao + "]";
 	}
 
 }
---
+++ cadastro/src/main/java/br/com/github/danielso/ifood/cadastro/mapper/IRestauranteMapper.java
@@ -0,0 +1,39 @@
+package br.com.github.danielso.ifood.cadastro.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+import br.com.github.danielso.ifood.cadastro.dto.AdicionarRestauranteDTO;
+import br.com.github.danielso.ifood.cadastro.dto.RestauranteDTO;
+import br.com.github.danielso.ifood.cadastro.entities.Restaurante;
+
+@Mapper(componentModel = "cdi")
+public interface IRestauranteMapper {
+
+	IRestauranteMapper INSTANCE = Mappers.getMapper(IRestauranteMapper.class);
+
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "dataCriacao", ignore = true)
+	@Mapping(target = "dataAtualizacao", ignore = true)
+	@Mapping(target = "pratos", ignore = true)
+	@Mapping(target = "localizacao.id", ignore = true)
+	@Mapping(target = "localizacao.dataCriacao", ignore = true)
+	@Mapping(target = "localizacao.dataAtualizacao", ignore = true)
+	@Mapping(target = "localizacao.restaurante", ignore = true)
+	public Restaurante toRestaurante(AdicionarRestauranteDTO dto);
+	
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "dataCriacao", ignore = true)
+	@Mapping(target = "dataAtualizacao", ignore = true)
+	@Mapping(target = "pratos", ignore = true)
+	@Mapping(target = "localizacao.id", ignore = true)
+	@Mapping(target = "localizacao.dataCriacao", ignore = true)
+	@Mapping(target = "localizacao.dataAtualizacao", ignore = true)
+	@Mapping(target = "localizacao.restaurante", ignore = true)
+	public void toRestaurante(AdicionarRestauranteDTO dto, @MappingTarget Restaurante entity);
+
+	public RestauranteDTO toRestauranteDTO(Restaurante entity);
+
+}
